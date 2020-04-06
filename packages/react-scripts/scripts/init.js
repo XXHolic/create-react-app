fc27692ba36d05b93a6db6b745c541cda1ead304
thy <972143891@qq.com>
2020-04-06 14:33:56 +0800

    docs: add comment

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -350,6 +350,9 @@ module.exports = function(
 
   console.log();
   console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log(chalk.red("TIP"));
+  console.log(' Code Fun! ');
+  console.log();
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
